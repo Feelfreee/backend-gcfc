71ab3bf16b68fabe783f011bc41015f367149cf9
Nilanjan Majumdar <n.majumder14@gmail.com>
2021-05-29 08:20:24 +0530

    intial start

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
 const express = require('express');
 
 const {getAllUsers,getDefault,getUser} = require("./handlers/demo");
-const {fct_login} = require("./handlers/user");
+const {fct_login,get_post} = require("./handlers/user");
 
 // Create an Express object and routes (in order)
 const app = express();
 app.use('/users/firebase_login', fct_login);
+app.use('/users/get_post', get_post);
 app.use(getDefault);
 
 // Set our GCF handler to our Express app.
