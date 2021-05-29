6687f3ab927c3a2e49402031f27af42b5f87aa9c
Nilanjan Majumdar <n.majumder14@gmail.com>
2021-05-29 17:01:40 +0530

    create_chat_room code deployed

--- handlers/user.js
+++ handlers/user.js
@@ -187,9 +187,123 @@ mutation {
 
  }
 
+ const create_room = async (req,res) => {
+
+  const { post_id, user_id, helper_id } = req.body.input;
+
+ const hasura_check_if_room_exists = `
+query MyQuery {
+room_candidates(where: {post_id: {_eq: "${post_id}"}, user_id: {_eq: "${helper_id}"}}) {
+  room_id
+}
+}
+`
+ 
+const hasura_create_room = `
+mutation {
+insert_rooms_one(object: {}) {
+  id
+}
+}
+`
+
+ 
+const fetch_rooms = await fetch(
+  "https://feelfree12.herokuapp.com/v1/graphql",
+  {
+    method: 'POST',
+    body: JSON.stringify({
+      query: hasura_check_if_room_exists
+    }),
+    headers: { 'Content-Type': 'application/json',
+               'x-hasura-admin-secret':'25a779ba-116e-47a2-9272-458f30af0449'
+},
+  }
+);
+
+  const room_data = await fetch_rooms.json();
+ //console.log("asas",room_data.data.room_candidates[0])
+  
+ 
+ 
+  if(room_data.data.room_candidates[0] !== undefined){
+    const room_id = room_data.data.room_candidates[0].room_id;
+    return res.status(200).json({
+      roomId:room_id
+    })
+  }else{
+    const create_room = await fetch(
+  "https://feelfree12.herokuapp.com/v1/graphql",
+  {
+    method: 'POST',
+    body: JSON.stringify({
+      query: hasura_create_room
+    }),
+    headers: { 'Content-Type': 'application/json',
+               'x-hasura-admin-secret':'25a779ba-116e-47a2-9272-458f30af0449'
+},
+  }
+);
+    
+    const room_details = await create_room.json();
+    //console.log(room_details);
+    const room_id = room_details.data.insert_rooms_one.id
+    
+    const hasura_create_room_candidate_add_user = `
+mutation {
+insert_room_candidates_one(object: {created_by: "${user_id}", post_id: "${post_id}", room_id: "${room_id}", user_id: "${user_id}"}) {
+  room_id
+}
+}
+`
+    
+    const hasura_create_room_candidate_add_helper = `
+mutation {
+insert_room_candidates_one(object: {created_by: "${user_id}", post_id: "${post_id}", room_id: "${room_id}", user_id: "${helper_id}"}) {
+  room_id
+}
+}
+`
+    
+  const create_room_candidate_add_user = await fetch(
+  "https://feelfree12.herokuapp.com/v1/graphql",
+  {
+    method: 'POST',
+    body: JSON.stringify({
+      query: hasura_create_room_candidate_add_user
+    }),
+    headers: { 'Content-Type': 'application/json',
+               'x-hasura-admin-secret':'25a779ba-116e-47a2-9272-458f30af0449'
+},
+  }
+);
+    
+  const create_room_candidate_add_helper = await fetch(
+  "https://feelfree12.herokuapp.com/v1/graphql",
+  {
+    method: 'POST',
+    body: JSON.stringify({
+      query: hasura_create_room_candidate_add_helper
+    }),
+    headers: { 'Content-Type': 'application/json',
+               'x-hasura-admin-secret':'25a779ba-116e-47a2-9272-458f30af0449'
+},
+  }
+);
+    
+    console.log(create_room_candidate_add_helper,create_room_candidate_add_user);
+    
+    return res.status(200).json({
+      roomId:room_id
+    })
+    
+  }
+
+}
 
   module.exports = {
    fct_login,
    get_post,
-   help_in_post
+   help_in_post,
+   create_room
       }
--- index.js
+++ index.js
@@ -2,12 +2,14 @@
 const express = require('express');
 
 const {getAllUsers,getDefault,getUser} = require("./handlers/demo");
-const {fct_login,get_post} = require("./handlers/user");
+const {fct_login,get_post,help_in_post,create_room} = require("./handlers/user");
 
 // Create an Express object and routes (in order)
 const app = express();
 app.use('/users/firebase_login', fct_login);
 app.use('/users/get_post', get_post);
+app.use('/users/help_in_post', help_in_post);
+app.use('/users/create_room', create_room);
 app.use(getDefault);
 
 // Set our GCF handler to our Express app.
